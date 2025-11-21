29b16c1c5a41b84dd2cc3ae79a8f59fa9de8f1e7
Maha Abo Assi <eng.mahaab96@gmail.com>
2025-11-21 10:10:36 +0400

    fix the font size inside the metrics cards for small screen

--- src/sections/about.tsx
+++ src/sections/about.tsx
@@ -48,7 +48,7 @@ const About: React.FC =()=>{
                         {e.signal && <span className="text-xl">{e.signal}</span>}
                         </h3>
                     </div>
-                    <p className=" text-lg ">{e.desc}</p>
+                    <p className="text-xs xs:text-sm md:text-lg ">{e.desc}</p>
                     </div>
                 ))}
             </motion.div>
